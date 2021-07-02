1e10f109e57a47ec98e2bee88db7b0d9364e6341
marcus.santos <mpsoulbass@gmail.com>
2021-07-01 22:05:49 -0300

    feat: Create customer model tests

--- sales/customers/tests.py
+++ sales/customers/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from model_mommy import mommy
 
-# Create your tests here.
+from sales.customers.models import Customer, CustomerCard
+
+
+class CustomerTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.customer = mommy.make(Customer)
+
+    def test_create(self):
+        self.assertEqual(Customer.objects.count(), 1)
+        self.assertIsNotNone(self.customer)
+
+    def test_update(self):
+        old_cpf = self.customer.cpf
+        self.customer.cpf = "00000000000"
+        self.customer.save()
+
+        self.assertNotEqual(self.customer.cpf, old_cpf)
+
+    def test_delete(self):
+        CustomerCard.objects.create(
+            customer=self.customer,
+            number="000000000000000"
+        )
+        cid = self.customer.id
+        self.assertGreater(CustomerCard.objects.filter(customer_id=cid).count(), 0)
+
+        self.customer.delete()
+
+        self.assertEqual(Customer.objects.count(), 0)
+        self.assertEqual(CustomerCard.objects.filter(customer_id=cid).count(), 0)
