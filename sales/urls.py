f38e63bca31fe21227d76962cad5fad510b298cd
marcus.santos <mpsoulbass@gmail.com>
2021-07-02 00:14:46 -0300

    ref: Change api doc routes

--- sales/urls.py
+++ sales/urls.py
@@ -24,20 +24,15 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("api/", include(router.urls)),
+    path("", include(router.urls)),
     re_path(
-        r'^swagger(?P<format>\.json|\.yaml)$',
+        r'^schema(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0),
         name='schema-json'
     ),
     path(
-        'swagger/',
+        'doc/',
         schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'
-    ),
-    path(
-        'redoc/',
-        schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'
-    ),
+    )
 ]
