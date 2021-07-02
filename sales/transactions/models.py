c4e587f4d685b81e272ef1d406377b885778501e
marcus.santos <mpsoulbass@gmail.com>
2021-07-01 22:14:28 -0300

    feat: Transaction implementation

--- sales/transactions/models.py
+++ sales/transactions/models.py
@@ -21,3 +21,21 @@ class TransactionType(BaseModel):
 
     def __str__(self):
         return self.description
+
+
+class Transaction(BaseModel):
+    date = models.DateField("Data da Compra")
+    hour = models.TimeField("Hora da Compra")
+    value = models.DecimalField("Valor da Compra", decimal_places=2, max_digits=10)
+
+    # Relationships
+    type = models.ForeignKey(TransactionType, on_delete=models.PROTECT, null=False)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    customer_card = models.ForeignKey(CustomerCard, on_delete=models.SET_NULL, null=True)
+    store = models.ForeignKey(Store, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"[{self.date} {self.hour}] {self.type}, {self.customer}, {self.store}"
+
+    class Meta:
+        unique_together = ["date", "hour", "value", "type", "customer", "customer_card"]
