a425aba07c175c88be3a8c50e5b7f8f23395b10b
marcus.santos <mpsoulbass@gmail.com>
2021-07-01 22:08:47 -0300

    feat: Store implementation

--- sales/stores/models.py
+++ sales/stores/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Manager, Q
 
 from sales.core.models import BaseModel
 
@@ -8,3 +9,25 @@ class StoreOwner(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class StoreFilterManager(Manager):
+
+    def search(self, query):
+        queryset = super(StoreFilterManager, self).get_queryset()
+        return queryset.filter(
+            Q(name__icontains=query) | Q(owner__name__icontains=query)
+        ).order_by("name").distinct()
+
+
+class Store(BaseModel):
+    name = models.CharField("Nome", max_length=60, null=False)
+    owner = models.ForeignKey(StoreOwner,
+                              related_name="stores",
+                              on_delete=models.PROTECT,
+                              null=False)
+
+    objects = StoreFilterManager()
+
+    def __str__(self):
+        return f"<{self.owner}> {self.name}"
