1edec271dfdf8fb5a9103037f36a1d473268f02a
marcus.santos <mpsoulbass@gmail.com>
2021-07-01 21:54:51 -0300

    feat: CustomerCard implementation

--- sales/customers/models.py
+++ sales/customers/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 from sales.core.models import BaseModel
 
+
+class CustomerCard(BaseModel):
+    customer = models.ForeignKey("Customer",
+                                 related_name="credit_cards",
+                                 on_delete=models.CASCADE,
+                                 null=False)
+    number = models.CharField("Card Number", max_length=15, null=False)
+
+
 class Customer(BaseModel):
     cpf = models.CharField("CPF", max_length=11, null=False, unique=True)
 
